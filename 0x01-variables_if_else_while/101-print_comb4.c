d85589d793bbd65d5a54047cd96244f2d94217dc
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-14 15:43:57 -0700

    10

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+        int one;
+        int tow;
+	int thr;
+
+for (one = 0; one <= 9; one++)
+{
+        for (tow = 0; tow <= 9; tow++)
+        {
+		for (thr = 0; thr <=9; thr++)
+		{
+			if (!((teen == one) || (teen < one) || (tow > thr) || (one > tows)))
+			{
+                        	putchar(one + '0');
+                        	putchar(tow + '0');
+				putchar(thr + '0');
+                        	if (!(tow == 8 && one == 7 && thr == 9))
+                        	{
+                                	putchar(',');
+                                	putchar(' ');
+                        	}
+                	}	
+        	}	
+	}
+}
+putchar('\n');
+return (0);
+}
