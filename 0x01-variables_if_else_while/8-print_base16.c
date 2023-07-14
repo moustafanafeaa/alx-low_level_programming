c8431fd28c70110719dfd7a74b232e63488338fe
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-14 14:32:11 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	char c;
+
+for (n = 0; n < 10; n++)
+{
+	putchar(n + '0');
+}
+for (c = 'a'; c <= 'f' < c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
