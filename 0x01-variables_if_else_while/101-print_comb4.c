afe1542bf97fe41fb3e6f10335fc4c145c71a149
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-14 15:53:22 -0700

    10

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,29 +6,29 @@
  */
 int main(void)
 {
-        int one;
-        int tow;
-	int thr;
+int one;
+int tow;
+int thr;
 
 for (one = 0; one <= 9; one++)
 {
-        for (tow = 0; tow <= 9; tow++)
-        {
-		for (thr = 0; thr <=9; thr++)
+for (tow = 0; tow <= 9; tow++)
+{
+for (thr = 0; thr <= 9; thr++)
+{
+	if (!((tow == one) || (tow < one) || (tow > thr) || (one == tow)))
+	{
+		putchar(one + '0');
+		putchar(tow + '0');
+		putchar(thr + '0');
+		if (!(tow == 8 && one == 7 && thr == 9))
 		{
-			if (!((tow == one) || (tow < one) || (tow > thr) || (one > tow)))
-			{
-                        	putchar(one + '0');
-                        	putchar(tow + '0');
-				putchar(thr + '0');
-                        	if (!(tow == 8 && one == 7 && thr == 9))
-                        	{
-                                	putchar(',');
-                                	putchar(' ');
-                        	}
-                	}	
-        	}	
+			putchar(',');
+			putchar(' ');
+		}
 	}
+}
+}
 }
 putchar('\n');
 return (0);
