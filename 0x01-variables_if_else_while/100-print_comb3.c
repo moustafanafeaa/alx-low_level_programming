736bdcd1b1410184be85041bf5f7edc148783507
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-14 15:13:13 -0700

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int one;
+	int teen;
+
+for (one = 0; one <= 9; one++)
+{
+	for (teen = 0; teen <= 9; teen++)
+	{
+		if (!((teen == one) || (teen < one)))
+		{
+			putchar(teen + '0');
+			putchar(one + '0');
+			if (!((teen == 8) && (one == 9)))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
