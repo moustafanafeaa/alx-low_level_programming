9a9f2a0758167077bae63b7d20935c50bf65210b
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-18 14:46:16 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print alphabet in 10 lines from a - z
+ */
+void print_alphabet_x10(void)
+{
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
