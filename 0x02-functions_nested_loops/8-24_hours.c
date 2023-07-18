512ecd5891556121f9d45e349310bb852cf26222
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-18 16:47:02 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,12 +9,22 @@ void jack_bauer(void)
 	int i, j, k, m;
 
 	for (i = 0; i <= 2; i++)
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j <= 23; j++)
 			for (k = 0; k <= 5; k++)
 				for (m = 0; m <= 9; m++)
 				{
 					_putchar(i + '0');
-					_putchar(j + '0');
+					if (j > 10)
+					{
+						int res;
+
+						res = j % 10;
+						_putchar(res + '0');
+					}
+					else
+					{
+						_putchar(j + '0');
+					}
 					_putchar(':');
 					_putchar(k + '0');
 					_putchar(m + '0');
