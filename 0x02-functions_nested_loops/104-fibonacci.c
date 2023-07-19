ee42653530b692728e4c3acd6ee0a8b9daee2af2
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-19 08:09:43 -0700

    adv

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -33,7 +33,7 @@ int numLength(int n)
 int main(void)
 {
 	int count, init0s;
-	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f10 = 0, f20 = 1, sumo = 0;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f10 = 0, f20 = 0, sumo = 0;
 
 for (count = 1; count <= 98; count++)
 {
@@ -54,8 +54,8 @@ for (count = 1; count <= 98; count++)
 	sum = (f1 + f2) % mx;
 	sumo = f10 + f20 + (f1 + f2) /  mx;
 	f1 = f2;
-	f2 = sum;
 	f10 = f20;
+	f2 = sum;
 	f20 = sumo;
 
 	if (count != 98)
