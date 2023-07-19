89577f1a283dc7f63900a357b67d82c6735e6a72
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-19 12:34:53 -0700

    adv

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -10,21 +10,21 @@
 
 int convert_day(int month, int day)
 {
-    switch (month)
-    {
-        case 2:
-            day = 31 + day;
-            break;
-        case 3:
-            day = 59 + day;
-            break;
-        case 4:
-            day = 90 + day;
-            break;
-        case 5:
-            day = 120 + day;
-            break;
-        case 6:
+	switch (month)
+	{
+	case 2:
+		day = 31 + day;
+		break;
+	case 3:
+		day = 59 + day;
+		break;
+	case 4:
+		day = 90 + day;
+		break;
+	case 5:
+		day = 120 + day;
+		break;
+	case 6:
 		day = 151 + day;
 		break;
 	case 7:
@@ -47,6 +47,6 @@ int convert_day(int month, int day)
 		break;
 	default:
 		break;
-}
+	}
 return (day);
 }
