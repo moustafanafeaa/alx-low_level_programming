0a77c1dae5d6f620c80c1b9ae42b3027e27de83a
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-20 05:02:36 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry of program
+ *
+ * Return: return 0 seccess
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+return (0);
+}
