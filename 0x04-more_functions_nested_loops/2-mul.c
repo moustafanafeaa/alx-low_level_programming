6860ea9651167534250da08fe198504f5b4ac93f
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-19 21:19:12 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ *
+ * @c: check for input
+ *
+ * Return: Returns 1 if c is a digit
+ * Returns 0 otherwise
+ *
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	putchar(mul + 48);
+}
