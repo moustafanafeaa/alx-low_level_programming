5f03b549a4f8f5b438161adb0cc6f2e9991520c9
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-20 06:03:02 -0700

    9

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,9 +11,9 @@ void print_triangle(int size)
 
 if (size > 0)
 {
-	for (i = 0; i <= size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = i; j <= size; j++)
+		for (j = 1; j <= size; j++)
 		{
 			if ((i * j) <= size)
 			{
