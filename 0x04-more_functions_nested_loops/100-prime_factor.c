23ddf99c9f719a6141fef32c86193b565532564a
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-20 06:34:17 -0700

    11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - prints the numbers from 1 to 100, followed by a new line.
- *  But for multiples of three print Fizz instead of the number and for the
- *   multiples of five print Buzz. For numbers which are
- *   multiples of both three and five print FizzBuzz.
+ * main - entry point
  *
  * Return: return 0 seccess
  *
@@ -24,7 +21,7 @@ int main(void)
 			}
 		}
 		d = n / i;
-		n = d
+		n = d;
 	}
 	return (0);
 }
