4bffa7e93a7eb8e53a4d292a1a049bf6dbc96d8b
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-24 13:49:18 -0700

    1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,7 @@ void puts_half(char *str)
 	while (str[i])
 		++i;
 	if (i % 2 == 0)
-		n = i - 1 / 2;
+		n = (i - 1) / 2;
 	for (i = n; str[i] != '\0'; i++)
 		printf("%c", str[i]);
 printf("\n");
