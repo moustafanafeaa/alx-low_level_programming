aa49f858e2273bb1d3ad07ba9f0ce0578f6d81be
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-24 12:43:16 -0700

    1

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts -  returns the length of a string.
+ *
+ * @str: input pointer to a
+ *
+ */
+void _puts(char *str)
+{
+	printf("%p\n", *str);
+}
