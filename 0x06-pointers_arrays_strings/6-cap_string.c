df2bd9d3742e1fbe8ae50e5c565421f9c0e47c6f
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-27 05:44:39 -0700

    3

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,6 +2,44 @@
 #include <stdio.h>
 
 /**
+ * isLower -  function that check if the c is lower case
+ *
+ * @c: parameter to func
+ *
+ * Return: return 1 or 0
+ */
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isDelimiter -  function that capitalizes all words of a string.
+ *
+ * @c: parameter to func
+ *
+ * Return: return 
+ *
+ */
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = " \t\n, .!?\"{}{}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+	return (0);
+}
+
+
+
+
+
+
+
+
+/**
  * *cap_string -  function that capitalizes all words of a string.
  *
  * @str: destination parameter to func
