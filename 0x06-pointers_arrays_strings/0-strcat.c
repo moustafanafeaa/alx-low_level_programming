f7fff584ace37dbfc31a476ea30e14a63a1c7b9f
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-27 04:51:12 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,8 +4,8 @@
 /**
  * *_strcat - that concatenates two strings.
  *
- * dest: destination parameter to func
- * src: destination parameter to func
+ * @dest: destination parameter to func
+ * @src: destination parameter to func
  *
  * Return: pointer dest
  */
@@ -14,9 +14,9 @@ char *_strcat(char *dest, char *src)
 	int c, c1;
 
 	c = 0;
-	while(dest[c])
+	while (dest[c])
 		c++;
-	for(c1 = 0; src[c1]; c1++)
+	for (c1 = 0; src[c1]; c1++)
 		dest[c++] = src[c1];
 	return (dest);
 }
