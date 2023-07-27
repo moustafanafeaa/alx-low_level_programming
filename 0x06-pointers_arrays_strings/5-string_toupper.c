8ef1a63047163526261438b38ed4f653ff5188f7
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-27 05:30:21 -0700

    3

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *string_toupper -  changes all lowercase letters of a string to uppercase.
+ *
+ * @str: destination parameter to func
+ *
+ * Return: return (reverse of str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+return (str)
+}
