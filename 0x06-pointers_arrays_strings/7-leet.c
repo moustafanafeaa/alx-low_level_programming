75e5c28db2afbe423d74dc166bebfda80744d66e
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-27 14:47:09 -0700

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *leet -  that encodes a string into 1337.
+ *
+ * @c: destination parameter to func
+ *
+ * Return: return (reverse of str
+ */
+char *leet(char *c)
+{
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++;
+	}
+	return (cp);
+}
