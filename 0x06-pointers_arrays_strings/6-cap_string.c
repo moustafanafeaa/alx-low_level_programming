56edf17b95476b67e3808da7526d5957c2786da6
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-27 05:59:49 -0700

    3

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -48,7 +48,7 @@ char *cap_string(char *s)
 	{
 		if (isDelimiter(*s))
 			foundDelimit = 1;
-		else if (isDelimiter(*s) && foundDelimit)
+		else if (isLower(*s) && foundDelimit)
 		{
 			*s -= 32;
 			foundDelimit = 0;
