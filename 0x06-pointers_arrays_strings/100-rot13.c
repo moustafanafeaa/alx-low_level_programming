fa581d8e43f8944c18961d82f9c20623de70f6c5
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-27 14:59:25 -0700

    7

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *rot13 - encodes a string using rot13.
+ *
+ * @c: destination parameter to func
+ *
+ * Return: return (reverse of str
+ */
+char *rot13(char *c)
+{
+	int i;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = c;
+
+	while (*c)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*c == rot13[i])
+			{
+				*c = ROT13[i];
+				break;
+			}
+		}
+		c++
+	}
+	return (ptr);
+}
