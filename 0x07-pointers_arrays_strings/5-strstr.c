e3cac801d3beb4a999b0d5c51df36fd5c4c7e031
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-07-31 11:29:08 -0700

    4

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_strstr - function that locates a substring.
+ *
+ * @haystack: string thst scanned
+ * @needle: char that search from
+ *
+ * Return: return string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	char *p;
+
+	for (i = 0; haystack[i] >= '\0'; i++)
+	{
+		int j = 0;
+		for (;needle[j] != '\0'; j++)
+		{	
+			if (needle[j] == haystack[i])
+			{
+				p = haystack[j];
+				return (p);
+			}
+		}
+	}
+	return (0);
+}				
