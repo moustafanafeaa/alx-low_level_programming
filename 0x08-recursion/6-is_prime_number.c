aff28b3edc640e2cf64fa1297db993b852dde30f
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-02 05:55:24 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,16 +7,29 @@
  *
  * Return: return 1 if n is prime or 0 is not
  */
+int check_num(int n, int i);
 int is_prime_number(int n)
 {
-	int i = 2;
+	return (check_num(n, 2));
+}
 
-	if (n == 0 || n == 1)
+/**
+ * check_num -  function that returns 1 if the input integer is
+ *     a prime number, otherwise return 0.
+ *
+ * @n: input number
+ * @i: equal 2 counter
+ *
+ * Return: return 1 if n is prime or 0 is not
+ */
+int check_num(int n, int i)
+{
+	if (n <= 0 || n == 1)
 		return (0);
 	if (n == i)
 		return (1);
 	if (n % i == 0)
 		return (0);
 	i++;
-	return (is_prime_number(n));
+	return (check_num(n, i));
 }
