bf74176f312ccc70dfa41fe8070a50ca41b45854
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-01 22:36:03 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] <= '\0'; i++)
-		_putchar(s[i] + '0');
-	_putchar('\n');
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else 
+		_putchar('\n');
 }
