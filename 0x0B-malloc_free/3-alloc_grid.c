78895d8c6993a0f1ad2684fb401c8979968f7064
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-07 14:30:20 -0700

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,6 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	arr = malloc(height * sizeof(int));
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(int));
@@ -26,9 +25,6 @@ int **alloc_grid(int width, int height)
 			free(arr);
 			return (NULL);
 		}
-	}
-	for (i = 0; i < height; i++)
-	{
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 	}
