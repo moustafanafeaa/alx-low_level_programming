da569025d53670f18f37f190696e71fd96401005
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-10 10:56:27 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *_mset(char *str, char x, unsigned int num);
+/**
+ * _mset - fulls memory
+ * @str: pointer
+ * @x: char equal
+ * @num:size
+ * Return: return pointer
+ */
+char *_mset(char *str, char x, unsigned int num)
+{
+	char *po = str;
+
+	while (num--)
+		*str++ = x;
+	return (po);
+}
 /**
  * _calloc - function that allocates memory for an array, using malloc.
  * @size: unsigned int size of arr.
@@ -9,19 +26,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
-	unsigned int i;
+	void *p;
 
 	if (nmemb == 0 || size == 0)
 		return (0);
 
-	p = malloc(sizeof(unsigned int) * nmemb);
+	p = malloc(sizeof(int) * nmemb);
 	if (p == NULL)
 		return (0);
 
-	for (i = 0; i < size; i++)
-	{
-		p[i] = 0;
-	}
+	_mset(p, 0, sizeof(int) * nmemb);
+
 return (p);
 }
