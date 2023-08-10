0f448f63357cf9cb1e1da2cbc681fd2239860cae
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-10 11:05:50 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -26,7 +26,7 @@ char *_mset(char *str, char x, unsigned int num)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
