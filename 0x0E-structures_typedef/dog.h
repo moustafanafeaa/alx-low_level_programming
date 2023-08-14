de6605633b424f0c105984b36feb8245cc738541
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-14 16:17:54 -0700

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: string of name
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * desc: dog
+ */
 struct dog{
 	char *name;
 	float age;
 	char* owner;
 };
+#endif
