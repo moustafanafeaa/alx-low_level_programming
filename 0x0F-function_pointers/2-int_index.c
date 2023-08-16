d80fdbceeebf2374783fff706447fcfe22b4ec96
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-16 10:47:56 -0700

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index -that searches for an integer.
+ * @array: array of int
+ * @size: size of arr
+ * @cmp: ptr to func 
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && size && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
