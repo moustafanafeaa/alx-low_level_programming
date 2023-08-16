19d4579faeca6f3066e350e23a5203aa354dccc8
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-16 12:31:34 -0700

    2

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,7 @@
 #include "3-clc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * main - entry point check
  * @argc: num of args
@@ -7,7 +10,7 @@
  */
 int main(int args, int **argv)
 {
-	int (*op)(int, int), a, b;
+	int (*op_func)(int, int), a, b;
 	
 	if (argv != 4)
 		printf("Error\n", exit(98));
@@ -15,11 +18,11 @@ int main(int args, int **argv)
 	b = atoi(argv[3]);
 
 	op = get_op_func(argv[2]);
-	if (!op)
+	if (!op_func)
 		printf("Error\n", exit(99));
 
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 		printf("Error\n", exit(100));
-	printf("%d\n", op(a, b));
+	printf("%d\n", op_func(a, b));
 	return (0);
 }
