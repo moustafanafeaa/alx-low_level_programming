dd7e723c074a4553b38bda78dd21b59d5d2ea32c
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-16 08:58:23 -0700

    0

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - that prints a name.
+ * @name: name str
+ * @f: pointer to unction 
+ * Return: return int
+ */
+void print_name(char *name, void (*f)(char *));
+{
+	if (name && f)
+		f(name);
+}
