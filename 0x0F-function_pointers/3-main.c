dede5ab4b2f2f6e04fd724f806429f55296d88b9
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-16 19:41:29 -0700

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "3-clc.h"
+#include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -8,21 +8,21 @@
  * @argv: arr arg
  * Return: 0
  */
-int main(int args, int **argv)
+int main(int argc, char **argv)
 {
 	int (*op_func)(int, int), a, b;
 	
-	if (argv != 4)
-		printf("Error\n", exit(98));
+	if (argc != 4)
+		printf("Error\n"), exit(98);
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	op = get_op_func(argv[2]);
+	op_func = get_op_func(argv[2]);
 	if (!op_func)
-		printf("Error\n", exit(99));
+		printf("Error\n"), exit(99);
 
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-		printf("Error\n", exit(100));
+		printf("Error\n"), exit(100);
 	printf("%d\n", op_func(a, b));
 	return (0);
 }
