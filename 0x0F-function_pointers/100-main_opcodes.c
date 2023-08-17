b0713e2dd444d7378abb4b6c0606759dc091aa9e
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-16 20:01:26 -0700

    3

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: num of args
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	char *ptr = (char *)main;
+	int x;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	x = atoi(argv[1]);
+	if (x < 0)
+		printf("Error\n"), exit(2);
+
+	while (x--)
+		printf("%02hhx%s", *ptr++, x ? " " : "\n");
+	return (0);
+}
