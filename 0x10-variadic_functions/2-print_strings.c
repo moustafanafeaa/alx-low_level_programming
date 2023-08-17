9efa07f11067f43e77bb875f26f9bf63bc4117b3
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-17 14:36:31 -0700

    1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,4 +8,18 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	
+int i = n;
+va_list args;
+char *s;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(args, n);
+	while (i--)
+		printf("%s%s", (s = va_arg(args, char *)) ? s : "(nil)",
+				i ? (separator ? separator : "") : "\n");
+va_end(args);
+}	
