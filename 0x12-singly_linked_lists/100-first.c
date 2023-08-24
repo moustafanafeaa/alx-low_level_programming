312bae56e39d1bf93173bcb5de2f1ca886f6a52b
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-24 13:53:08 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - executes befor main()
+ */
+void _constuctor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
