ab7be238993479f4bbab2ce75a4ad47e5316d371
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-24 13:04:19 -0700

    0

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_node_end - add node to ls
+ * @head: address
+ * @str: str
+ * Return: return int
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_head = malloc(sizeof(list_t));
+	list_t *node = *head;
+
+	if (!head || !new_head)
+		return (0);
+	if (str)
+	{
+		new_head->str = strdup(str);
+		if (!new_head->str)
+		{
+			free(new_head);
+			return (NULL);
+		}
+		new_head->len = strlen(new_head->str);
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next new_node;
+	}
+	else
+		*head = new_head;
+	return (new_head);
+{
