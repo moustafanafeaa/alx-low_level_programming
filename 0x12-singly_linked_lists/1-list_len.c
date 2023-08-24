fa9fc00a82f8efc384728497926fba0a6e5fa60e
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-24 12:29:50 -0700

    0

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - return list len
+ * @h: s
+ * Return : return int
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
