ac146eacaba9a997fa1c5887e47e718177c8082d
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-08-28 15:03:57 -0700

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,9 +3,9 @@
  * free_listint2 - remove all nodes
  * @head: pointer to 1st node
  */
-void free_listint(listint_t **head)
+void free_listint2(listint_t **head)
 {
-	listint_t *nodeو *temp;
+	listint_t *node, *temp;
 
 	if (head == NULL)
 		return;
