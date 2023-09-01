28190f00e5b58741b21258308a7fc37765777331
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-09-01 04:35:59 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,12 +8,13 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
 
-	if (!b || *b != '0' || *b != '1')
+	if (!b)
 		return (0);
-	else
-		while(*b)
-		{
-			num = num * 2 + (*b++ - '0');
-		}
+	while(*b)
+	{
+		if (*b != '0' || *b != '1')
+			return (0);
+		num = num * 2 + (*b++ - '0');
+	}
 	return (num);
 }
